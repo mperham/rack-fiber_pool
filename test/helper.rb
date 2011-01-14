fd9d707e236ec2f7f180a4451084571e0a8f8c73
Mike Perham <mperham@gmail.com>
2011-01-13 17:27:02 -0800

    Add simple test cases

--- test/helper.rb
+++ test/helper.rb
@@ -1,2 +1,4 @@
 require 'minitest/autorun'
+require 'rack'
+require 'rack/mock'
 require 'rack/fiber_pool'
--- test/test_rack-fiber_pool.rb
+++ test/test_rack-fiber_pool.rb
@@ -3,6 +3,26 @@ require 'helper'
 class TestRackFiberPool < MiniTest::Unit::TestCase
 
   def test_usage
-    fail 'TODO'
+    app = TestApp.new
+    catch :async do
+      res = Rack::MockRequest.new(Rack::FiberPool.new(app)).get("/")
+    end
+    assert_equal [200, {"Content-Type"=>"text/plain"}, ["Hello world!"]], app.result
+  end
+
+  def test_size
+    app = TestApp.new
+    catch :async do
+      res = Rack::MockRequest.new(Rack::FiberPool.new(app, :size => 5)).get("/")
+    end
+    assert_equal [200, {"Content-Type"=>"text/plain"}, ["Hello world!"]], app.result
+  end
+  
+  class TestApp
+    attr_accessor :result
+    def call(env)
+      env['async.callback'] = proc { |result| @result = result }
+      [200, {"Content-Type" => "text/plain"}, ["Hello world!"]]
+    end
   end
 end
