27fc4e1bb97b1c8e17b1240a9a29a59d45e5bef2
François de Metz <fdemetz@af83.com>
2011-08-05 17:10:08 +0200

    By default rescue exception occuring within a fiber.
    Add rescue_exception option to override the default behavior.
    
    Signed-off-by: François de Metz <fdemetz@af83.com>
    Signed-off-by: Jean-Denis Vauguet <jdvauguet@af83.com>

--- test/test_rack-fiber_pool.rb
+++ test/test_rack-fiber_pool.rb
@@ -17,7 +17,32 @@ class TestRackFiberPool < MiniTest::Unit::TestCase
     end
     assert_equal [200, {"Content-Type"=>"text/plain"}, ["Hello world!"]], app.result
   end
-  
+
+  def test_exception
+    app = TestBuggyApp.new
+    catch :async do
+      res = Rack::MockRequest.new(Rack::FiberPool.new(app)).get("/")
+    end
+    assert_equal [500, {}, ""], app.result
+  end
+
+  def test_custom_rescue_exception
+    app = TestBuggyApp.new
+    catch :async do
+      rescue_exception = Proc.new { |env, exception| [503, {}, exception.message] }
+      res = Rack::MockRequest.new(Rack::FiberPool.new(app, :rescue_exception => rescue_exception)).get('/')
+    end
+    assert_equal [503, {}, "I'm buggy! Please fix me."], app.result
+  end
+
+  class TestBuggyApp
+    attr_accessor :result
+    def call(env)
+      env['async.callback'] = proc { |result| @result = result }
+      raise Exception.new("I'm buggy! Please fix me.")
+    end
+  end
+
   class TestApp
     attr_accessor :result
     def call(env)
